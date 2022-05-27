34ba62146cda9c388a7f318c9bc3e97371d98f6d
seok <dhdustnr0134@naver.com>
2022-05-27 12:28:06 +0900

    [sally4405-#4] Feat: 위시리스트 삭제 기능 Mock API 구현

---
+++ BE/src/main/java/louie/dong/airbnb/wishlist/MockWishListService.java
@@ -0,0 +1,11 @@
+package louie.dong.airbnb.wishlist;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MockWishListService {
+
+    public void delete(Long id) {
+
+    }
+}
