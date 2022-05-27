a85d9eba4b719267318c0dc9e8aed2cdd2abf819
donggi-lee-bit <devdonggilee@gmail.com>
2022-05-27 11:53:10 +0900

    [#4] Refactor: API 설계에 따라 도메인 객체 수정
    - 기존의 AccommodationController를 RegionController로 변경
    - PopularDestination을 TravelController로 변경
    - dto를 도메인에 맞게 수정하여 해당 도메인 패키지 하위 dto 패키지로 이동

--- BE/src/main/java/louie/dong/airbnb/travel/MockTravelService.java
+++ BE/src/main/java/louie/dong/airbnb/travel/MockTravelService.java
@@ -1,22 +1,22 @@
 package louie.dong.airbnb.travel;
 
 import java.util.List;
-import louie.dong.airbnb.dto.PopularDestinationResponse;
+import louie.dong.airbnb.travel.dto.PopularTravelResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MockTravelService {
 
-    public List<PopularDestinationResponse> findAll() {
+    public List<PopularTravelResponse> findAll() {
         return List.of(
-            new PopularDestinationResponse("서울", "", "차로 30분 거리"),
-            new PopularDestinationResponse("광주", "", "차로 4시간 거리"),
-            new PopularDestinationResponse("의정부시", "", "차로 30분 거리"),
-            new PopularDestinationResponse("수원시", "", "차로 45분 거리"),
-            new PopularDestinationResponse("대구", "", "차로 3.5시간 거리"),
-            new PopularDestinationResponse("울산", "", "차로 4.5시간 거리"),
-            new PopularDestinationResponse("대전", "", "차로 2시간 거리"),
-            new PopularDestinationResponse("부천시", "", "차로 30분 거리")
+            new PopularTravelResponse("서울", "", "차로 30분 거리"),
+            new PopularTravelResponse("광주", "", "차로 4시간 거리"),
+            new PopularTravelResponse("의정부시", "", "차로 30분 거리"),
+            new PopularTravelResponse("수원시", "", "차로 45분 거리"),
+            new PopularTravelResponse("대구", "", "차로 3.5시간 거리"),
+            new PopularTravelResponse("울산", "", "차로 4.5시간 거리"),
+            new PopularTravelResponse("대전", "", "차로 2시간 거리"),
+            new PopularTravelResponse("부천시", "", "차로 30분 거리")
         );
     }
 }
--- BE/src/main/java/louie/dong/airbnb/travel/TravelController.java
+++ BE/src/main/java/louie/dong/airbnb/travel/TravelController.java
@@ -2,7 +2,7 @@ package louie.dong.airbnb.travel;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import louie.dong.airbnb.dto.PopularDestinationResponse;
+import louie.dong.airbnb.travel.dto.PopularTravelResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TravelController {
 
-    private final MockTravelService mockPopularDestinationService;
+    private final MockTravelService mockTravelService;
 
     @GetMapping("/travel/popular")
-    public List<PopularDestinationResponse> getPopularDestinations() {
-        return mockPopularDestinationService.findAll();
+    public List<PopularTravelResponse> getPopularDestinations() {
+        return mockTravelService.findAll();
     }
 }
