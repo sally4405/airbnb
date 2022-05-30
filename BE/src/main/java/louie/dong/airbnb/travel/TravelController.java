bbf9a5f7c6863c36e961cbaa9a27fd1db2e34fdb
donggi-lee-bit <devdonggilee@gmail.com>
2022-05-31 07:53:34 +0900

    [sally4405/#4] Refactor: 인기 여행지 조회 방식 변경
    - Service 계층을 통해 반환하고 있던 인기 여행지 조회 API를 고정 데이터라고 판단하여 Controller에서 바로 반환하도록 변경

--- BE/src/main/java/louie/dong/airbnb/travel/TravelController.java
+++ BE/src/main/java/louie/dong/airbnb/travel/TravelController.java
@@ -1,19 +1,24 @@
 package louie.dong.airbnb.travel;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import louie.dong.airbnb.travel.dto.PopularTravelResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class TravelController {
-
-    private final MockTravelService mockTravelService;
 
     @GetMapping("/travel/popular")
     public List<PopularTravelResponse> getPopularDestinations() {
-        return mockTravelService.findAll();
+        return List.of(
+            new PopularTravelResponse("서울", "", "차로 30분 거리"),
+            new PopularTravelResponse("광주", "", "차로 4시간 거리"),
+            new PopularTravelResponse("의정부시", "", "차로 30분 거리"),
+            new PopularTravelResponse("수원시", "", "차로 45분 거리"),
+            new PopularTravelResponse("대구", "", "차로 3.5시간 거리"),
+            new PopularTravelResponse("울산", "", "차로 4.5시간 거리"),
+            new PopularTravelResponse("대전", "", "차로 2시간 거리"),
+            new PopularTravelResponse("부천시", "", "차로 30분 거리")
+        );
     }
 }
