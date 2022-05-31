1f1745bb80426c3998dee91676b33ffe1b2d2ece
donggi-lee-bit <devdonggilee@gmail.com>
2022-05-31 14:02:54 +0900

    [sally4405/#4] Feat: LocalDate 형식 변경을 위한 어노테이션 추가
    - API 스펙에 맞게끔 LocalDate 형식을 변경

--- BE/src/main/java/louie/dong/airbnb/accommodation/dto/AccommodationDetailPriceRequest.java
+++ BE/src/main/java/louie/dong/airbnb/accommodation/dto/AccommodationDetailPriceRequest.java
@@ -3,12 +3,15 @@ package louie.dong.airbnb.accommodation.dto;
 import java.time.LocalDate;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
 public class AccommodationDetailPriceRequest {
 
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate checkIn;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private LocalDate checkOut;
 	private int guestCount;
 }
