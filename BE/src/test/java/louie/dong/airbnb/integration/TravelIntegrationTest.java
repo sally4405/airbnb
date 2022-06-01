6418de45945a36f271b39ca5fa8a1029050b1cb3
donggi-lee-bit <devdonggilee@gmail.com>
2022-06-01 14:59:54 +0900

    [sally4405/#13] Fix: 테스트 코드에 imageUrl 추가
    Co-authored-by: Louie-03 <dhdustnr0134@naver.com>

--- BE/src/test/java/louie/dong/airbnb/integration/TravelIntegrationTest.java
+++ BE/src/test/java/louie/dong/airbnb/integration/TravelIntegrationTest.java
@@ -48,17 +48,17 @@ class TravelIntegrationTest {
             .filter(document("get-popular-destination", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint())))
 
             .when()
-            .get("/travel/popular")
+            .get("/travels/popular")
 
             .then()
             .statusCode(HttpStatus.OK.value())
             .assertThat()
             .body("", hasSize(8))
             .body("[0].name", equalTo("서울"))
-            .body("[0].imageUrl", equalTo(""))
+            .body("[0].imageUrl", equalTo("https://s3-alpha-sig.figma.com/img/4ee4/e169/870b792e3a4ae88671095fad825a8ef0?Expires=1655078400&Signature=EIrSNxPaFvj-98axTpoBNQce68DaGCdUpbcYfeX5SOATzix31nECjaroPUKlfrgPxsPVPNpWpxFd21d9C7p6MrWLucqKM3A-DMFcBzDeawce2ZL3h7Pvh4YP1Y8D6XE98pVgnp23t6a4~ZR8efMDUs2h3JHbIJ4Y7QTY2Tf4eYhWLypBOrNcxkIUwMyg5IzZEQ0IGGCV10z336zie-c3bybqHB~BvL2BHJMh1jR6qNH-2IhJgZW5FWmkC3YUy8PqmDwpc4aKXw8~2bwps6-k0WOAbAvdg~zKsuElJhnr~bl8vrHmLCg1LMloqxnccVIPNvskhfDMMrEhPBWFcg2aFQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"))
             .body("[0].distance", equalTo("차로 30분 거리"))
             .body("[1].name", equalTo("광주"))
-            .body("[1].imageUrl", equalTo(""))
+            .body("[1].imageUrl", equalTo("https://s3-alpha-sig.figma.com/img/d11b/fafa/041959faaab5d3d5bd0c98fc56ab6feb?Expires=1655078400&Signature=UVu97EeonTnwgVld1jInrE8CpEzktSbkkKBJuEyO47xUWSUKDkAZcuIRi1RGUIYuE4qt~Q9L2oUpJS03sqSNRPAZqbBG8qIuQt5Zdx705opWB2uw3yFuH8fRSsocbT6MmAOsRip3pNaGiC55Tf3VouO5hE-YYhC-7KtRBYhNuqM2-PCYbI~UlcqBJztv0Nbzp-~quisREFC37yS~u-rusJvqp675eyRWR8vOTWz0lZgpphWwjUCupbTzm7TvsinqKMQp90nx~evjleNmcEJxKwIT8Bb81LxYWXuJWVEoXatvZkmoOFP3kDaHdHC-SVrwxZRXdU3U0M-bfZ~WKPoyYg__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"))
             .body("[1].distance", equalTo("차로 4시간 거리"));
     }
 }
